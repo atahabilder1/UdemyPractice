dbaa498f200c65c2713a0f6ff3098ef29a27699b
Anik Tahabilder <Anik Tahabilder@35.16.8.237>
2022-02-06 16:21:34 -0500

    LinkedListcodedagain

--- UdemyPractice/src/sort_25/InsertionSort.java
+++ UdemyPractice/src/sort_25/InsertionSort.java
@@ -1,64 +1,70 @@
 package sort_25;
 
-
+ 
 
 public class InsertionSort{
-	//	This is the main method
+//	This is the main method
 	public static void main(String args[]) {
-		int givenArray[]= {2,6,5,16,3,21,9,4,78,54,40,33,15,};
+		int givenArray[]= {2,3,5,6,3,4,9,4,78,54,4};
 		// print before sorting
 		System.out.println("The array before sorting: ");
 		printArray(givenArray);
 		System.out.println();
-
-
+		
+		
 		// Execution of sorting operation
-		insertionSort(givenArray);
-
+		bubbleSort(givenArray);
+		
 		//	Array after sorting
 		System.out.println("The array after sorting: ");
 		printArray(givenArray);
 	}
-
-
-
-
-	// This is the insertion method to sort
-
-	public static int[] insertionSort(int array1[]) {
-		for (int i = 1; i < array1.length; i++) {
-			int temp=array1[i];
-			int j=i-1;
-				while((temp<array1[j]) && j>=0) {
-					array1[j+1]=array1[j];
-					j--;
+	
+	
+	
+	
+// This is the method to sort
+	
+	public static int[] bubbleSort(int array1[]) {
+		for (int i = 0; i < array1.length-1; i++) {
+			for (int j = 0; j < array1.length-1-i; j++) {
+				if (array1.length>1) {
+					
+					if (array1[j+1]<array1[j]) {
+						
+						// This is swaping the values
+//						int temp =array1[j+1];
+//						 array1[j+1]=array1[j];
+//						 array1[j]=temp;
+						swap(array1, j+1, j); // isntead of using this swap function we can compute it directly
+						
+						
+						
+						
+					}
+				}
 			}
-				array1[j+1]=temp;
-				 
-				 
-
-
 		}
 		return  array1;
-
+		
 	}
-
-
-// This is swap method defination  // I have not used this function here
-public static int[] swap(int c[] ,int a, int b) {
-	int temp;
-	temp=c[a];
-	c[a]=c[b];
-	c[b]=temp;
-	return c;  
-}
-
-
+	
+ 
+// This is swap method defination
+	public static int[] swap(int c[] ,int a, int b) {
+		 int temp;
+		 temp=c[a];
+		 c[a]=c[b];
+		 c[b]=temp;
+		return c;  
+	}
+	 
+	
 // This is the method to print
-public static void printArray(int inputarray[]) {
-	for (int i = 0; i < inputarray.length; i++) {
-		System.out.print( inputarray[i]+ ", ");
+	public static void printArray(int inputarray[]) {
+		for (int i = 0; i < inputarray.length; i++) {
+			System.out.print( inputarray[i]+ ", ");
+		}
+		
 	}
-
-}
 }
